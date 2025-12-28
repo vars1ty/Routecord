2e2e83c1ab952c5438a2ac5a33c38f76a046cf88
vars1ty <gh@resistant.ch>
2025-12-28 06:42:11 +0100

    config: Fix directory path

--- routecord/src/utils.rs
+++ routecord/src/utils.rs
@@ -11,9 +11,9 @@ impl Utils {
         let home_dir = home_dir.display();
 
         #[cfg(target_os = "linux")]
-        return std::path::Path::new(&format!("{home_dir}/.config/fercord")).to_path_buf();
+        return std::path::Path::new(&format!("{home_dir}/.config/routecord")).to_path_buf();
 
         #[cfg(target_os = "windows")]
-        std::path::Path::new(&format!("{home_dir}\\fercord")).to_path_buf()
+        std::path::Path::new(&format!("{home_dir}\\routecord")).to_path_buf()
     }
 }
